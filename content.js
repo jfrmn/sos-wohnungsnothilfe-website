8a654313f3c6595c2d7ad8192086158af73b4f2e
jofrmn <jfriedemann@proton.me>
2026-06-12 23:59:18 +0200

    added pages home, verein, wohnungslosigkeit and some javascript

---
+++ content.js
@@ -0,0 +1,49 @@
+
+async function setupSite() {
+	const queryParams = new URLSearchParams(window.location.search);
+
+	const page = queryParams.get("page");
+	
+	console.log("Page:", page);
+
+	let pageSpecifics = {};
+	if (page === "home" || page === null) {
+		pageSpecifics = {
+			heroBannerBackground: "assets/Bildwelt/PageBilder/Homepage_Hausschlüssel.png",
+			heroBannerContentHtml: "GEMEINSAM FÜR<br>NEUE PERSPEKTIVEN",
+			contentPage: "pages/home.html"
+		};
+	} else if (page === "wohnungslosigkeit") {
+		pageSpecifics = {
+			heroBannerBackground: "assets/Bildwelt/PageBilder/dimi-katsavaris-N3m2hKNc1j0-unsplash.jpg",
+			heroBannerContentHtml: "JEDER BRAUCHT<br>EIN ZUHAUSE",
+			contentPage: "pages/wohnungslosigkeit.html"
+		};
+	} else if (page === "verein") {
+		pageSpecifics = {
+			heroBannerBackground: "assets/Bildwelt/PageBilder/Gemini_Generated_Image_cdg9pmcdg9pmcdg9.png",
+			heroBannerContentHtml: "UM UNBÜROKRATISCH UND<br>UNKOMPLIZIERT HELFEN ZU KÖNNEN",
+			contentPage: "pages/verein.html"
+		};
+	}
+
+	const heroBanner = document.querySelector(".hero-banner");
+	heroBanner.style.backgroundImage = `url(${pageSpecifics.heroBannerBackground})`;
+	
+	const heroBannerContent = document.querySelector(".hero-banner-content-text");
+	heroBannerContent.innerHTML = pageSpecifics.heroBannerContentHtml;
+
+	const contentContainer = document.querySelector(".content");
+	const content = await fetch(pageSpecifics.contentPage);
+	if (!content.ok) {
+		contentContainer.innerHTML = `
+			<h1>404 - Seite nicht gefunden</h1>
+			<p>Die angeforderte Seite konnte nicht gefunden werden.</p>
+		`;
+		return;
+	}
+
+	contentContainer.innerHTML = await content.text();
+}
+
+window.addEventListener("DOMContentLoaded", setupSite);
---
+++ highlight-link.js
@@ -0,0 +1,52 @@
+
+const template = document.createElement('template');
+template.innerHTML = `
+	<style>		
+		.highlight-link {
+			display: inline-flex;
+			gap: 16px;
+			margin-bottom: 24px;
+		}
+
+		.highlight-link a {
+			color: #ffffff;
+			background-color: #22A9AB;
+			font-size: 25px;
+			text-decoration: none;
+			align-self: flex-end;
+			width: fit-content;
+		}
+
+		.highlight-link a:hover {
+			background-color: #A5DAEC;
+		}
+	</style>
+
+	<div class="highlight-link">
+		<img src="assets/Icons/link.svg" alt="Link Icon" />
+		<a href="#">
+			<slot></slot>
+		</a>
+	</div>
+`;
+
+class HighlightLink extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+		this.shadowRoot.appendChild(template.content.cloneNode(true));
+		this.linkElement = this.shadowRoot.querySelector('a');
+	}
+
+	static get observedAttributes() {
+		return ['href'];
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		if (name === 'href') {
+			this.linkElement.setAttribute('href', newValue);
+		}
+	}
+}
+
+customElements.define('highlight-link', HighlightLink);
